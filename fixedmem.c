f4b0dde73809f28c079428d21c280313a22e6849
David <gek@katherine>
2021-03-06 19:07:37 -0500

    Automatic commit.

--- fixedmem.c
+++ fixedmem.c
@@ -1,15 +1,20 @@
 #include "fixedmem.h"
 #include <stdio.h>
 
-//28 = 2^10 * 2^10 * 2^8, or 256 megabytes.
-FIXEDMEM_BLOCK_EXTERN(myblock, 28);
-FIXEDMEM_BLOCK(myblock, 28);
+//name, power of 2. MUST be large enough to hold at least one size_t.
+FIXEDMEM_BLOCK_EXTERN(myblock, 12);
+FIXEDMEM_BLOCK(myblock, 12);
 
 
 int main(){
+	//Check memory alignment.
+	if(0){
+		printf("Array:, %zx\n", (size_t)myblock_mem);
+		exit(1);
+	}
 	while(1)
 		for(size_t i = 0; i < myblock_size / (sizeof(double)); i++){
 			printf("Value at %zx is %f\n", i, ((double*)myblock(0))[i]  );
-			((double*)myblock(0))[i] = (double)i;
+			((double*)myblock(0))[i] += (double)i;
 		}
 }

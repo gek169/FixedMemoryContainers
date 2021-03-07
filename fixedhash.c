a0670ee78e3cc96d7314a26c0069e36c7636a183
David <gek@katherine>
2021-03-06 21:57:26 -0500

    Automatic commit.

--- fixedhash.c
+++ fixedhash.c
@@ -14,11 +14,11 @@ typedef struct {
 //Argument 5: The number of "columns" of the hashmap to search. if it's equal to 1<<(argument 3) then it is most
 //memory efficient, but the worst case scenario is slowest.
 //ZERO id is an invalid hashmap entry
-FIXEDMEM_HASHMAP(hdata, myhmap, 8, 4, 2);
-FIXEDMEM_HASHMAP_EXTERN(hdata, myhmap, 8, 4, 2);
+FIXEDMEM_HASHMAP(hdata, myhmap, 8, 2, 2);
+FIXEDMEM_HASHMAP_EXTERN(hdata, myhmap, 8, 2, 2);
 
 int main(){
-	for(size_t i = 1; i < 1239033; i+= 337){
+	for(size_t i = 1; i < 256*2+7; i++){
 		hdata mine;
 		mine.id = i;
 		mine.data = i<<1;
@@ -30,7 +30,7 @@ int main(){
 			printf("Cannot find a spot! i = %zu\n", i);
 		}
 	}
-	for(size_t i = 1; i < 1239033; i+= 337){
+	for(size_t i = 1; i < 256 *2+7; i++){
 		hdata* targ = myhmap_get(i);
 		if(targ){
 			printf("Found him! i = %zu, data = %u, id = %zu\n", i, targ->data, targ->id);
@@ -41,7 +41,7 @@ int main(){
 		}
 	}
 
-	for(size_t i = 1; i < 1239033; i+= 337){
+	for(size_t i = 1; i < 256 *2+7; i++){
 		hdata* targ = myhmap_get(i);
 		if(targ)
 			puts("Bad juju!");

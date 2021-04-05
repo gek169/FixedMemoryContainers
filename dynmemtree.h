8a039ab6bab310db0ce9395778ac41222c7309c5
MHS <gek@katherine>
2021-04-05 11:37:27 -0400

    Automatic commit.

--- dynmemtree.h
+++ dynmemtree.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-
+#include <assert.h>
 #ifndef DYNTREE_SIZE_T
 #define DYNTREE_SIZE_T size_t
 #endif
@@ -11,6 +11,37 @@
 #ifndef DYNTREE_FREE
 #define DYNTREE_FREE free
 #endif
+
+#ifndef DYNTREE_ALLOC
+#define DYNTREE_ALLOC malloc
+#endif
+
+#define TABLE(type, name, n)\
+typedef struct{type* d[ ((DYNTREE_SIZE_T)1<<(DYNTREE_SIZE_T)(n-1)) ];} name;\
+static const DYNTREE_SIZE_T name##_size = ((DYNTREE_SIZE_T)1<<(DYNTREE_SIZE_T)(n-1));\
+static void name##_init(name* f){\
+	assert(n > 0);\
+	for(DYNTREE_SIZE_T i = 0; i < ((DYNTREE_SIZE_T)1<<(DYNTREE_SIZE_T)(n-1)); i++)\
+		f->d[i] = NULL;\
+}\
+static type* name##_lazy_get(name* f, DYNTREE_SIZE_T i){/*Allocates if not available*/\
+	i &= ((DYNTREE_SIZE_T)1<<(DYNTREE_SIZE_T)(n-1)) - 1;\
+	if(f->d[i]) return f->d[i];\
+	f->d[i] = (type*)DYNTREE_ALLOC(sizeof(type));\
+	if(!f->d[i]) abort();/*Check that memory allocation succeeded- abort on failre.*/\
+	memset(f->d[i], 0, sizeof(type)); /*Check*/\
+	return f->d[i];\
+}\
+static type* name##_get(name* f, DYNTREE_SIZE_T i){/*Safe indexing only.*/\
+	i &= ((DYNTREE_SIZE_T)1<<(DYNTREE_SIZE_T)(n-1)) - 1;\
+	return f->d[i];\
+}\
+static void name##_cleanup(name* f){\
+	for(DYNTREE_SIZE_T i = 0; i < ((DYNTREE_SIZE_T)1<<(DYNTREE_SIZE_T)(n-1)); i++){\
+		if(f->d[i]) DYNTREE_FREE(f->d[i]);\
+	}\
+}
+
 
 #define DYNTREE(type, name, n)							\
 typedef struct{											\
@@ -37,7 +68,7 @@ static void name##_cleanup(name* f){						\
 		/*Bottom of the tree. no c.*/						\
 		if(cpr)cpr->c[cpi]	= NULL;							\
 		if(c->d)DYNTREE_FREE(c->d);							\
-		if(c != f) DYNTREE_FREE(c); else break;						\
+		if(c != f) DYNTREE_FREE(c); else break;				\
 		c = f; cpr = NULL; cpi = 0;							\
 	}														\
 															\
--- dyntree.c
+++ dyntree.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 #include "dynmemtree.h"
-
+//define types.
 DYNTREE(char*, mybin, 4);
+//Table type
+TABLE(unsigned int, mytable, 4);
+
 
 mybin b = (mybin){0, {0}}; //When declared global, it is already in its initialized state.
-
+//Demonstrate ordinary dyntree
 void create_c(mybin* t, uint8_t iter){
 	mybin* d1 = calloc(1, sizeof(mybin));
 	d1->d = calloc(1,10);
@@ -24,4 +27,12 @@ int main(){
 	b.d = calloc(1,200);
 	create_c(&b, 20);
 	mybin_cleanup(&b);
+	mytable q;
+	mytable_init(&q);
+	*mytable_lazy_get(&q, 3) = 5;
+	*mytable_lazy_get(&q, 1) = 7;
+	*mytable_lazy_get(&q, 0) = 9;
+	for(unsigned i = 0; i < mytable_size; i++)
+		if(mytable_get(&q, i))
+			printf("value at %u is is %u\n", i, *mytable_lazy_get(&q, i));
 }

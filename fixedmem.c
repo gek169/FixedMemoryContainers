13c0a36f4dfaa15a9c2e0d2b09f597931e36288f
MHS <gek@katherine>
2021-04-06 20:13:31 -0400

    Automatic commit.

--- fixedmem.c
+++ fixedmem.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 //name, power of 2. MUST be large enough to hold at least one size_t.
-FIXEDMEM_BLOCK_EXTERN(myblock, 28);
-FIXEDMEM_BLOCK(myblock, 28);
+FIXEDMEM_BLOCK_EXTERN(myblock, 32);
+FIXEDMEM_BLOCK(myblock, 32);
 
 
 int main(){
@@ -13,7 +13,7 @@ int main(){
 		exit(1);
 	}
 	while(1)
-		for(size_t i = 0; i < myblock_size / (sizeof(double)); i++){
+		for(size_t i = 0; i < myblock_size / (sizeof(double)); i+= 10000){
 			printf("Value at %zx is %f\n", i, ((double*)myblock(0))[i]  );
 			((double*)myblock(0))[i] += (double)i;
 		}
